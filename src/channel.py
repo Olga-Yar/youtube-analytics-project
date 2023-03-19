aec5fcef1da7807b77c1e5e42cabbcb812fc46cf
Olga Yarosh <O_ll_y_a@mail.ru>
2023-03-19 15:13:04 +0300

    correct structure

--- src/channel.py
+++ src/channel.py
@@ -4,14 +4,12 @@ from googleapiclient.discovery import build
 
 from helper.youtube_api_manual import youtube, api_key
 import json
-from pyyoutube import Api
 
 api_key: str = os.getenv('YT_API_KEY')
 
+
 class Channel:
     """Класс для ютуб-канала"""
-
-
 
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
@@ -30,12 +28,10 @@ class Channel:
         channel = youtube.channels().list(id=self.__channel_id, part='snippet,statistics').execute()
         return channel
 
-
     @classmethod
     def get_service(cls):
         youtube = build('youtube', 'v3', developerKey=api_key)
         return youtube
-
 
     def to_json(self, name):
         with open(name, 'w', encoding='utf-8') as outfile:
