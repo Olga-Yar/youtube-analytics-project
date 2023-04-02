6e5664c5ac4b83fd1d85ae4090bc96245bb2e83a
Olga Yarosh <O_ll_y_a@mail.ru>
2023-04-02 15:35:41 +0300

    change class Video

--- src/video.py
+++ src/video.py
@@ -2,15 +2,20 @@ from src.channel import youtube
 
 
 class Video:
-    def __init__(self, video_id, video_name, video_url, views, likes):
+    def __init__(self, video_id):
         self. video_id = video_id
-        self.video_name = video_name
-        self.video_url = video_url
-        self.views = views
-        self.likes = likes
+
+        video_response = youtube.videos().list(part='snippet,statistics,contentDetails,topicDetails',
+                                               id=video_id
+                                               ).execute()
+        print(video_response)
+        self.video_title: str = video_response['items'][0]['snippet']['title']
+        self.video_url: str = video_response['items'][0]['snippet']['thumbnails']['default']['url']
+        self.view_count: int = video_response['items'][0]['statistics']['viewCount']
+        self.like_count: int = video_response['items'][0]['statistics']['likeCount']
 
     def __str__(self):
-        return self.video_name
+        return self.video_title
 
 
 class PLVideo:
