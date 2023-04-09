ef5141e40180bf951adfa468a508f75c50c7ed05
Olga Yarosh <O_ll_y_a@mail.ru>
2023-04-09 15:27:59 +0300

    create try/except

--- src/video.py
+++ src/video.py
@@ -1,18 +1,26 @@
 from src.channel import youtube
+from googleapiclient.errors import HttpError
 
 
 class Video:
     def __init__(self, video_id):
-        self. video_id = video_id
+        try:
+            self.video_id = video_id
+            video_response = youtube.videos().list(part='snippet,statistics,contentDetails,topicDetails',
+                                                   id=video_id
+                                                   ).execute()
+            self.video_title: str = video_response['items'][0]['snippet']['title']
+            self.video_url: str = video_response['items'][0]['snippet']['thumbnails']['default']['url']
+            self.view_count: int = video_response['items'][0]['statistics']['viewCount']
+            self.like_count: int = video_response['items'][0]['statistics']['likeCount']
+        except HttpError as e:
+            print(e)
+            self.video_title: str = None
+            self.video_url: str = None
+            self.view_count: int = None
+            self.like_count: int = None
 
-        video_response = youtube.videos().list(part='snippet,statistics,contentDetails,topicDetails',
-                                               id=video_id
-                                               ).execute()
-        print(video_response)
-        self.video_title: str = video_response['items'][0]['snippet']['title']
-        self.video_url: str = video_response['items'][0]['snippet']['thumbnails']['default']['url']
-        self.view_count: int = video_response['items'][0]['statistics']['viewCount']
-        self.like_count: int = video_response['items'][0]['statistics']['likeCount']
+
 
     def __str__(self):
         return self.video_title
